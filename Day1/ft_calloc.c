b27497310bd59e32636132a6097f58a5155f198d
Bertrand <b.pouchepandin@gmail.com>
2020-07-11 17:28:11 +0200

    calloc.c

---
+++ Day1/ft_calloc.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpouchep <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/11 17:05:51 by bpouchep          #+#    #+#             */
+/*   Updated: 2020/07/11 17:05:53 by bpouchep         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	if ((ptr = (void *)malloc(size * count)) != NULL)
+	{
+		ft_bzero(ptr, size * count);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
