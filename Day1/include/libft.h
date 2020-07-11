4513cc19affba7e781555f96c95e9335149f452a
Bertrand <b.pouchepandin@gmail.com>
2020-07-11 17:29:36 +0200

    make libft.h update

--- Day1/include/libft.h
+++ Day1/include/libft.h
@@ -40,5 +40,6 @@ void		*ft_memmove(void *dst, const void *src, size_t n);
 void		ft_bzero(void *s, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 void		*ft_memchr(const void *str, int c, size_t n);
+void		*ft_calloc(size_t count, size_t size);
 
 #endif
