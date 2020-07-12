dad8c56a2153062b9255c08f0342fee5660be87e
kiGzz <azn_kiguel@hotmail.fr>
2020-07-12 23:14:53 +0200

    putnbr_fd

---
+++ Day1/ft_putnbr_fd.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpouchep <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/12 22:40:44 by bpouchep          #+#    #+#             */
+/*   Updated: 2020/07/12 22:40:45 by bpouchep         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long number;
+
+	number = n;
+	if (number < 0)
+	{
+		ft_putchar_fd('-', fd);
+		number *= -1;
+	}
+	if (number > 9)
+		ft_putnbr_fd(number / 10, fd);
+	ft_putchar_fd(number % 10 + 48, fd);
+}
