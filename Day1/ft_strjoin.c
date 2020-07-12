24287df50950a8bd33673e6c707447bba2beea07
kiGzz <azn_kiguel@hotmail.fr>
2020-07-12 22:00:41 +0200

    strjoin

--- Day1/ft_strjoin.c
+++ Day1/ft_strjoin.c
@@ -11,8 +11,31 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char		*ft_strjoin(char const *s1, char const *s2)
 {
+	char	*dest;
+	int		i;
+	int		end;
 
+	i = 0;
+	end = 0;
+	if (!(dest = (char*)malloc(ft_strlen(s1) + ft_strlen(s2))))
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		dest[i] = s1[i];
+		i++;
+		end = i;
+	}
+	i = 0;
+	while (s2[i] != '\0')
+	{
+		dest[end] = s2[i];
+		i++;
+		end++;
+	}
+	dest[end] = '\0';
+	return (dest);
 }
