0927ab33c986fb665eef7778659447288607a9b6
kiGzz <azn_kiguel@hotmail.fr>
2020-07-12 23:00:19 +0200

    putchar putendl putstr

--- Day1/libft.h
+++ Day1/libft.h
@@ -43,5 +43,9 @@ void		*ft_memchr(const void *str, int c, size_t n);
 void		*ft_calloc(size_t count, size_t size);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 char		*ft_strjoin(char const *s1, char const *s2);
+void 		ft_putchar_fd(char c, int fd);
+void		ft_putstr_fd(char *s, int fd);
+void		ft_putendl_fd(char *s, int fd);
+void		ft_putnbr_fd(int n, int fd);
 
 #endif
