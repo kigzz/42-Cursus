85fff494cfb3b2da26eca37d92651ec8ada49131
kiGzz <azn_kiguel@hotmail.fr>
2020-07-13 17:43:40 +0200

    strtrim

---
+++ Day1/ft_strtrim.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpouchep <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/13 16:56:21 by bpouchep          #+#    #+#             */
+/*   Updated: 2020/07/13 16:56:50 by bpouchep         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+// s'arrête à la première itération qui n'est pas dans le *set
+// Début + Fin
+// connaitre strlen de dest (Faire fonction?)
+// OU connaitre début + fin et substr?
+
+char		*ft_strtrim(char const *s1, char const *set)
+{
+	char	*dest;
+
+
+}
