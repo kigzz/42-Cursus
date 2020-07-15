f77d369d57bb21157cc70f1c6e4b100f63ecfdb8
kiGzz <azn_kiguel@hotmail.fr>
2020-07-15 23:17:08 +0200

    ft_itoa

--- Day1/ft_itoa.c
+++ Day1/ft_itoa.c
@@ -11,9 +11,55 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-char	*ft_itoa(int n)
+int			number_char(int n)
 {
+	int		len;
 
+	len = 0;
+	while (n > 0)
+	{
+		n /= 10;
+		len++;
+	}
+	return (len);
+}
+
+int			ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	else
+		return (nb * ft_recursive_power(nb, power - 1));
+}
+
+char		*ft_itoa(int n)
+{
+	char	*str;
+	int		len;
+	int		i;
+
+	len = number_char(n);
+	i = 0;
+	if (!(str = (char*)malloc(sizeof(char) * len + 1)))
+		return (NULL);
+	else
+	{
+		if (n < 0)
+		{
+			str[i] = 0;
+			i++;
+		}
+		while (len > 0)
+		{
+			str[i] = (n / ft_recursive_power(10, len - 1)) + '0';
+			n = n % ft_recursive_power(10, len - 1);
+			len--;
+			i++;
+		}
+	}
+	str[i] = '\0';
+	return (str);
 }
