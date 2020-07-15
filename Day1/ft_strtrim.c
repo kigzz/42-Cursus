f80d6ed04b3a69bfdcfccacc4ca99db4fb8155ac
kiGzz <azn_kiguel@hotmail.fr>
2020-07-15 19:13:23 +0200

    ft_strtrim.c

--- Day1/ft_strtrim.c
+++ Day1/ft_strtrim.c
@@ -6,35 +6,27 @@
 /*   By: bpouchep <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 16:56:21 by bpouchep          #+#    #+#             */
-/*   Updated: 2020/07/13 16:56:50 by bpouchep         ###   ########.fr       */
+/*   Updated: 2020/07/15 18:55:08 by bpouchep         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-// s'arrête à la première itération qui n'est pas dans le *set
-// Début + Fin
-// connaitre strlen de dest (Faire fonction?)
-// OU connaitre début + fin et return substr?
 
-int			start(char const *s1, char const *set) {
-	size_t i;
-	size_t j;
+int			start(char const *s1, char const *set)
+{
+	size_t	i;
+	size_t	j;
 
 	i = 0;
 	j = 0;
-	while (s1[i] != '\0') {
+	while (s1[i] != '\0' && j != ft_strlen(set))
+	{
 		j = 0;
-		while (set[j] != '\0' && s1[i] != set[j]) {
+		while (set[j] != '\0' && s1[i] != set[j])
 			j++;
-		}
-		if (j == ft_strlen(set))
-			break;
 		i++;
-		if (i == ft_strlen(s1))
-			return (0);
 	}
-	return (i);
+	return (i - 1);
 }
 
 int			end(char const *s1, char const *set)
@@ -44,36 +36,29 @@ int			end(char const *s1, char const *set)
 
 	i = ft_strlen(s1) - 1;
 	j = 0;
-	while (i > 0)
+	while (i > 0 && j != ft_strlen(set))
 	{
 		j = 0;
 		while (set[j] != '\0' && s1[i] != set[j])
 		{
 			j++;
 		}
-		if (j == ft_strlen(set))
-			break;
 		i--;
-		if (i == 0)
-			return (ft_strlen(s1));
 	}
-	printf("i_end = %ld\n", i);
-	return (i);
+	return (i + 1);
 }
 
-//char		*ft_strtrim(char const *s1, char const *set)
-//{
-//	char	*dest;
-//
-//}
-
-int main (void)
+char		*ft_strtrim(char const *s1, char const *set)
 {
-	char str[30] = "ABCABCAB";
-	char set[30] = "";
-	int i = start(str, set);
-	int j = end(str, set);
+	int		s;
+	int		e;
+	size_t	len;
 
-	printf("%d\n%c\n", i, str[i]);
-	printf("%d\n%c\n", j, str[j]);
+	s = start(s1, set);
+	e = end(s1, set);
+	if (s > e)
+		len = 0;
+	else
+		len = e - s + 1;
+	return (ft_substr(s1, s, len));
 }
