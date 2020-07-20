8a3de49d2f6f540c03a584db24addc716e92d7c4
kiGzz <azn_kiguel@hotmail.fr>
2020-07-20 22:52:56 +0200

    strncmp cast char to unsigned char in return

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
@@ -21,7 +22,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		}
 		i++;
 		n--;
