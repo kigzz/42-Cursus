f264e62fb8b92b8ded19a68167eb27860d2a6427
kiGzz <azn_kiguel@hotmail.fr>
2020-07-20 11:53:06 +0200

    atoi changes \t\v etc

--- Day1/ft_atoi.c
+++ Day1/ft_atoi.c
@@ -12,22 +12,24 @@
 
 #include "libft.h"
 
-int		ft_atoi(char *str)
+int	ft_atoi(char *str)
 {
-	int	result;
-	int	i;
-	int	sign;
+	int result;
+	int i;
+	int sign;
 
 	i = 0;
 	result = 0;
 	sign = 1;
-	if (str[i] == '-')
+	while ((str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f'
+			|| str[i] == '\r' || str[i] == ' '))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
 	{
-		sign = -1;
+		if (str[i] == '-')
+			sign = sign * -1;
 		i++;
 	}
-	else if (str[i] == '+')
-		i++;
 	while (str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
 	{
 		result = result * 10 + str[i] - '0';
