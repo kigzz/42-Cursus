a5d0dd3cc3d24140cf7a6457ea2ae1cfe796af1b
kiGzz <azn_kiguel@hotmail.fr>
2020-07-23 19:13:36 +0200

    strtrim fix

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -12,53 +12,19 @@
 
 #include "libft.h"
 
-static int	start(char const *s1, char const *set)
+char		*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	i;
 	size_t	j;
 
 	i = 0;
-	j = 0;
-	while (s1[i] != '\0' && j != ft_strlen(set))
-	{
-		j = 0;
-		while (set[j] != '\0' && s1[i] != set[j])
-			j++;
+	j = ft_strlen(s1) - 1;
+	while (s1[i] && ft_strchr(set, s1[i]))
 		i++;
-	}
-	return (i - 1);
-}
-
-static int	end(char const *s1, char const *set)
-{
-	size_t	i;
-	size_t	j;
-
-	i = ft_strlen(s1) - 1;
-	j = 0;
-	while (i > 0 && j != ft_strlen(set))
-	{
-		j = 0;
-		while (set[j] != '\0' && s1[i] != set[j])
-		{
-			j++;
-		}
-		i--;
-	}
-	return (i + 1);
-}
-
-char		*ft_strtrim(char const *s1, char const *set)
-{
-	int		s;
-	int		e;
-	size_t	len;
-
-	s = start(s1, set);
-	e = end(s1, set);
-	if (s > e)
-		len = 0;
+	while (s1[j] && i < j && ft_strchr(set, s1[j]))
+		j--;
+	if (i >= j)
+		return (ft_strdup(""));
 	else
-		len = e - s + 1;
-	return (ft_substr(s1, s, len));
+		return (ft_substr(&s1[i], 0, j + 1 - i));
 }
