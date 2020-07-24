f1393c5647a07d72e8eeb3daecd2960127659517
kiGzz <azn_kiguel@hotmail.fr>
2020-07-24 16:43:17 +0200

    let's go

---
+++ GNL/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpouchep <bpouchep@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/24 16:37:58 by bpouchep          #+#    #+#             */
+/*   Updated: 2020/07/24 16:38:34 by bpouchep         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h?>
+
+int		get_next_line(int fd, char **line);
+
+#endif
