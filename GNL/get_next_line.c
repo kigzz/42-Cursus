2a664f57eb695683e8a740120ba729d82ce1c4c6
kiGzz <azn_kiguel@hotmail.fr>
2020-08-24 23:35:13 +0200

    expliations str1 - str2

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -22,7 +22,7 @@ int				get_next_line(int fd, char **line)
 
 	if (tmpline == NULL)
 		tmpline = "";
-	printf("strchr = %s\n-------\n", nl);
+//==	printf("strchr = %s\n-------\n", nl);
 //		printf("Index = %d | Line[%d] = %s\n", index, i, line[i]);
 //	fd = open("42", O_RDWR);
 //	printf("fd = %d\n", fd);
@@ -34,13 +34,13 @@ int				get_next_line(int fd, char **line)
 		tmpline = nl + 1;
 		if (ft_strchr(tmpline, '\n'))				//CONDITION A REVOIR !!
 		{
-			printf("tmpline if NL présent : %s\n", tmpline);
+//===			printf("tmpline if NL présent : %s\n", tmpline);
 			line[i] = ft_strndup(tmpline, ft_strchr(tmpline, '\n') - tmpline);
-			printf("line : %s\n", line[i]);
+//==			printf("line : %s\n", line[i]);
 			i++;
 		}
 		nl = ft_strchr(tmpline, '\n');
-		printf("nl if nl != null : %s\n", nl);
+//==		printf("nl if nl != null : %s\n", nl);
 	}
 	while ((index = read(fd, buf, BUFFER_SIZE)) && !(nl = ft_strchr(buf, '\n')))
 	{
@@ -48,15 +48,13 @@ int				get_next_line(int fd, char **line)
 			return (-1);
 		}
 		buf[index] = '\0';
-		printf("buf = %s\n", buf);
-		printf("before tmpline : %s\n", tmpline);
 		if (!tmpline) {
 			tmpline = ft_strndup(buf, BUFFER_SIZE);
-			printf("if !(tmpline) = %s\n", tmpline);
+//==			printf("if !(tmpline) = %s\n", tmpline);
 		}
 		else {
 			tmpline = ft_strjoin(tmpline, buf);
-			printf("if (tmpline) = %s\n", tmpline);
+//==			printf("if (tmpline) = %s\n", tmpline);
 		}
 	}
 
@@ -65,19 +63,20 @@ int				get_next_line(int fd, char **line)
 	{
 //		printf("index = %ld\n", index);
 //		printf("i = %d\n", i);
-//		printf("buf = %s\n", buf);
+		printf("nl = %s\n", nl);
+		printf("buf = %s\n", buf);
 //		printf("strjoin = %s\n", ft_strjoin(buf, nl));
-//		printf("strndup = %s\n", ft_strndup(buf, nl - buf));
-//		printf("strchr - buf = %ld\n", nl - buf);				//	Différence de nombre de char de nl par à rapport à buf;
+		printf("strndup = %s\n", ft_strndup(buf, nl - buf));	//		nl pointe à l'endroit 5. buf pointe à l'endroit 0
+		printf("strchr - buf = %ld\n", nl - buf);
 //		tmpline = ft_strjoin(tmpline, (ft_strndup(buf, nl - buf)));
 		tmpline = ft_strjoin(tmpline, ft_strndup(buf, nl - buf));
 	}
 //	printf("5index = %ld\n", index);
-	printf("index = %ld\n", index);
-	printf("FINAL TMPLINE = %s\n", tmpline);
+//	printf("index = %ld\n", index);
+//==	printf("FINAL TMPLINE = %s\n", tmpline);
 	line[i] = tmpline;
 	i++;
-	return (0);
+	return (1);
 }
 
 int				main(void)
