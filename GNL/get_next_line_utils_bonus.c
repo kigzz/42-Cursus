a0dcb4beb6d34641c183b3f10e847ba19b6d25b5
kiGzz <azn_kiguel@hotmail.fr>
2021-04-29 03:45:33 +0200

    bonus, norminette, test

---
+++ GNL/get_next_line_utils_bonus.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bpouchep <bpouchep@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/04/29 03:02:41 by bpouchep          #+#    #+#             */
+/*   Updated: 2021/04/29 03:02:43 by bpouchep         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t			ft_strlen(const char *str)
+{
+	const char	*str2;
+
+	str2 = str;
+	while (*str != '\0')
+		str++;
+	return (str - str2);
+}
+
+char			*ft_strndup(const char *src, size_t n)
+{
+	char	*dest;
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	if (ft_strlen(src) > n)
+		len = n;
+	else
+		len = ft_strlen(src);
+	if (!(dest = malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	while (i < len)
+	{
+		dest[i] = *src++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char			*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*str;
+	size_t			i;
+	const size_t	str_len = ft_strlen(s1) + ft_strlen(s2);
+
+	if (!(str = malloc((str_len + 1) * sizeof(char))))
+		return (NULL);
+	i = 0;
+	while (*s1)
+		str[i++] = *s1++;
+	while (*s2)
+		str[i++] = *s2++;
+	str[i] = '\0';
+	return (str);
+}
+
+char			*ft_strchr(const char *str, int c)
+{
+	int i;
+
+	i = 0;
+	if (!str)
+		return (NULL);
+	while (str[i] != c)
+	{
+		if (str[i] == '\0')
+			return (NULL);
+		i++;
+	}
+	return ((char*)&str[i]);
+}
